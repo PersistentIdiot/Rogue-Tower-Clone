5268d1566aa15728d9bee83f193d86fd2a5f87c1
JonBrant <jonbrant@gmail.com>
2023-03-23 07:02:02 -0400

    Dead end checking

--- Assets/_RogueTowerClone/Scripts/CameraDrag.cs
+++ Assets/_RogueTowerClone/Scripts/CameraDrag.cs
@@ -5,34 +5,48 @@ using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
 {
-    [SerializeField] private float sensitivity = 1;
+    [SerializeField] private float moveSensitivity = 0.1f;
+    [SerializeField] private float zoomSensitivity = 1;
+    [SerializeField] private float yMin = 5;
+    [SerializeField] private float yMax = 200;
     Vector3 lastMousePosition;
 
     private void Update()
     {
-        MouseInputs();
+        MovementInputs();
+        ScrollInputs();
     }
 
-    void MouseInputs()
+    private void ScrollInputs()
+    {
+        float delta = Input.mouseScrollDelta.y;
+        Vector3 newPosition = transform.position;
+        newPosition.y = Mathf.Clamp(newPosition.y - delta * zoomSensitivity, yMin, yMax);
+
+        transform.position = newPosition;
+    }
+
+    private void MovementInputs()
     {
         if (Input.GetMouseButtonDown(0))
         {
             lastMousePosition = Input.mousePosition;
         }
-        else if(Input.GetMouseButton(0))
+        else if (Input.GetMouseButton(0))
         {
-            Vector3 delta = (Input.mousePosition - lastMousePosition)*sensitivity;
- 
+            Vector3 delta = (Input.mousePosition - lastMousePosition) * moveSensitivity;
+
             MoveCamera(delta.x, delta.y);
- 
+
             lastMousePosition = Input.mousePosition;
         }
     }
+
     private void MoveCamera(float xInput, float zInput)
     {
         float zMove = Mathf.Cos(transform.eulerAngles.y * Mathf.PI / 180) * zInput - Mathf.Sin(transform.eulerAngles.y * Mathf.PI / 180) * xInput;
         float xMove = Mathf.Sin(transform.eulerAngles.y * Mathf.PI / 180) * zInput + Mathf.Cos(transform.eulerAngles.y * Mathf.PI / 180) * xInput;
- 
+
         transform.position = transform.position + new Vector3(-xMove, 0, -zMove);
     }
 }
--- Assets/_RogueTowerClone/Scripts/Map.cs
+++ Assets/_RogueTowerClone/Scripts/Map.cs
@@ -10,6 +10,7 @@ using UnityEngine.UI;
 public class Map : MonoBehaviour
 {
     [SerializeField] private Block startingBlock;
+    [SerializeField] private Block deadEndBlock;
     [SerializeField] private List<Block> blockPrefabs = new List<Block>();
 
     [Header("UI - Temporary")]
@@ -27,7 +28,7 @@ public class Map : MonoBehaviour
         SpawnButtons();
     }
 
-    
+
     // ToDo: Check if start intersects previous path and replace with a new EndBlock prefab
     // ToDo: Create a new path List<Vector3> for mobs to traverse from each VALID endpoint
     public void SpawnNewBlock(int endPoint = 0)
@@ -40,8 +41,6 @@ public class Map : MonoBehaviour
         // Move it into the correct position
         Vector3 direction = (lastBlock.endPoints[endPoint].position - lastBlock.transform.position).normalized;
         newBlock.transform.position = lastBlock.transform.position + direction * 11;
-
-
 
         // Orient new block ( via Nom - https://discord.com/channels/750329891383410728/983851080255418408/1088325758050648134 )
         newBlock.transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
@@ -57,10 +56,42 @@ public class Map : MonoBehaviour
             newBlock.transform.rotation *= Quaternion.AngleAxis(180, Vector3.forward);
         }
 
+
+        // Check for dead ends - BUGGED!! Always claims a dead end
+        foreach (Transform newBlockEndPoint in newBlock.endPoints)
+        {
+            //RaycastHit[] hits = Physics.RaycastAll(raycastOrigin + Vector3.up, Vector3.down, 5);
+            Collider[] hits = Physics.OverlapSphere(newBlockEndPoint.transform.position, 5);
+            Debug.Log($"Hits: {hits.Length}");
+            foreach (Collider hit in hits)
+            {
+                //Debug.Log($"Hit: {hit.gameObject.name}");
+                if (hit.TryGetComponent(out Block hitBlock))
+                {
+                    Debug.Log($"Hit block");
+                    if (hitBlock == newBlock)
+                    {
+                        continue;
+                    }
+                    Debug.Log($"DeadEnd!");
+
+                    var rot = newBlock.transform.rotation;
+                    var pos = newBlock.transform.position;
+                    
+                    DestroyImmediate(newBlock);
+                    newBlock = Instantiate(deadEndBlock, transform);
+                    newBlock.transform.rotation = rot;
+                    newBlock.transform.position = pos;
+                    break;
+                }
+            }
+
+        }
+
         lastBlock = newBlock;
         SpawnButtons();
     }
-    
+
     // ToDo: Keep a list of all valid End points, this only checks the last placed block
     private void SpawnButtons()
     {
@@ -76,7 +107,7 @@ public class Map : MonoBehaviour
         {
             // Spawn new buttons
             Vector3 direction = (lastBlock.endPoints[i].position - lastBlock.transform.position).normalized;
-            var spawnPosition =  lastBlock.transform.position + direction * 11;
+            var spawnPosition = lastBlock.transform.position + direction * 11;
             var buttonPosition = lastBlock.transform.position + direction * 11 + new Vector3(0, 5, 0);
 
             // Check if position would result in a collision with an existing block
